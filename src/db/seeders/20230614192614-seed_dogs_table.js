c9a6d5d77c1a197aa121fe0bd5511a2f12d7562e
Abubeker Seifu <abubeker_seifu@et.jsi.com>
2023-06-14 23:50:36 +0300

    Add seeder to populate initial dog records

---
+++ src/db/seeders/20230614192614-seed_dogs_table.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('dogs', [
+      { name: 'Neo', color: 'red&amber', tail_length: 22, weight: 32 },
+      { name: 'Jessy', color: 'black&white', tail_length: 7, weight: 14 }
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+  }
+};
